9ce6c00374780e951a60a173ec306c27d00e12ff
Jacob Alex Foster <jacob.afoster@icloud.com>
2025-06-17 09:31:12 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+print("Input Opposite:")
+a = input()
+a = int(a) * int(a)
+print("Input Adjacent:")
+b = input()
+b = int(b) * int(b)
+c = a + b
+print("Hypotenuse is equal to:",math.sqrt(c))
